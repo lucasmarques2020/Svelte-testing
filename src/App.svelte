00e911e02129ac99ef19f1f8034d1dd49f9a516d
Lucas Marques <luquinhas.mb07@gmail.com>
2021-08-23 11:44:04 -0300

    first project in svelte

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+import Botao from "./components/Botao.svelte";
+
+</script>
+
+<main>
+	<Botao N_btn="7"></Botao>
+	<Botao N_btn="8"></Botao>
+	<Botao N_btn="9"></Botao>
+	<Botao N_btn="4"></Botao>
+	<Botao N_btn="5"></Botao>
+	<Botao N_btn="6"></Botao>
+	<Botao N_btn="1"></Botao>
+	<Botao N_btn="2"></Botao>
+	<Botao N_btn="3"></Botao>
+</main>
+
+<style>
+	*{
+		padding: 0;
+		margin: 0;
+	}
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: blue;
+		height: 100%;
+		width: 100%;
+		text-align: center;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Botao.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    function exibir(){
+        alert("oi, fui clicado!");
+    }
+    export let N_btn = '';
+</script>
+
+<button class = "botao" on:click={exibir}>{N_btn}</button>
+
+<style>
+    .botao{
+        padding: 20px;
+        height: 20px;
+        width: 20px;
+        color: white;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: black;
+        transition: 0.3s;
+    }
+    .botao:hover{
+        color: white;
+        padding: 20px;
+        background-color: rgb(255, 6, 6);
+    }
+</style>
